2a62a81a77d5e47780e609ba3ac83a13f0872e2a
thatsbass <bassdiaw.study@outlook.com>
2024-08-13 10:58:05 -0400

    server

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: `https://backendg7.onrender.com`,
+        url: `https://backendg7-mqwx.onrender.com`,
         description: 'Serveur de développement',
       },
       
