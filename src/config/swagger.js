65623f3b62b5636bbf06da12310d1112123ff4ef
thatsbass <bassdiaw.study@outlook.com>
2024-08-13 11:29:34 -0400

    release

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: `https://backendg7-mqwx.onrender.com`,
+        url: `https://threadly.onrender.com`,
         description: 'Serveur de développement',
       },
       
