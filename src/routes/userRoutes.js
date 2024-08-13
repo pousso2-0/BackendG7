feb5babd012e8ed76034dcdd1bd75698f9a987e3
Mamefatou05 <fatouniangbasse@gmail.com>
2024-08-13 07:49:16 +0000

    params

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,7 +9,6 @@ import MesureController from '../controllers/mesureController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
-
 /**
  * @swagger
  * /api/users/register:
@@ -22,17 +21,41 @@ const router = express.Router();
  *         application/json:
  *           schema:
  *             type: object
- *            properties:
- *               username:
+ *             properties:
+ *               name:
  *                 type: string
+ *                 description: Nom complet de l'utilisateur
  *               email:
  *                 type: string
+ *                 format: email
+ *                 description: Adresse email de l'utilisateur
  *               password:
  *                 type: string
- *               
- * 
- *   
- *               
+ *                 description: Mot de passe de l'utilisateur
+ *               type:
+ *                 type: string
+ *                 description: Type d'utilisateur (ex. : "admin", "client", etc.)
+ *               profilePicture:
+ *                 type: string
+ *                 format: uri
+ *                 description: URL de la photo de profil de l'utilisateur (optionnel)
+ *               bio:
+ *                 type: string
+ *                 description: Biographie ou description de l'utilisateur (optionnel)
+ *               location:
+ *                 type: string
+ *                 description: Emplacement ou ville de l'utilisateur (optionnel)
+ *               gender:
+ *                 type: string
+ *                 description: Genre de l'utilisateur (optionnel)
+ *               phone:
+ *                 type: string
+ *                 description: Numéro de téléphone de l'utilisateur (optionnel)
+ *               skills:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 description: Liste des compétences de l'utilisateur (optionnel)
  *     responses:
  *       200:
  *         description: Utilisateur créé avec succès
